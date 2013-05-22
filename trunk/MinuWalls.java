e0d879d6334e76a1bf9e16e79900cda5fe92fb86
indraneel.tambe.1 <indraneel.tambe.1@ada5487b-2e95-c5f6-e23e-4c46d1c84117>
2013-05-22 00:57:07 +0000

    Fixed Minu's horrible InstructionManual code

--- trunk/MinuWalls.java
+++ trunk/MinuWalls.java
@@ -1,22 +1,21 @@
 // Driver for MinuWalls.
-import java.lang.Object.*; 
 import javax.swing.JOptionPane; 
 
 
 public class MinuWalls {
      
- public static void main(String [] args) {
-  JOptionPane i = new JOptionPane();
-  System.setProperty("info.gridworld.gui.selection", "hide");
-  InstructionManual s = new InstructionManual(i);
-  s.showInstruction();
-
-  MWWorld game = new MWWorld();
-  game.startNew();
-  game.show();
+	public static void main(String [] args) {
+		JOptionPane optionPane = new JOptionPane();
+		System.setProperty("info.gridworld.gui.selection", "hide");
+		InstructionManual manual = new InstructionManual(optionPane);
+		manual.showInstructions();
+		
+		MWWorld game = new MWWorld();
+		game.startNew();
+		game.show();
+     	
      
-     
- }
+	}
      
 }
 
