d7156f00bd936e3386c361220a9b30dbee744fd0
hans04219@gmail.com <hans04219@gmail.com@ada5487b-2e95-c5f6-e23e-4c46d1c84117>
2013-05-22 20:47:12 +0000

    edited comments

--- NormalizePowerup.java
+++ NormalizePowerup.java
@@ -1,16 +1,16 @@
 import java.awt.Color;
 
-public class NormalizePowerup extends MinuPowerup {
+public class NormalizePowerup extends MinuPowerup {//extends the abstract class
 
 	public NormalizePowerup(){
-		this.setColor(Color.magenta); //color change
+		this.setColor(Color.magenta); //sets color to magenta
 	}
      
 	public void act() {} // it stays in place
 
 	public void editPlayer(MinuWaller player) {
-		player.setDelay(2); //2 STEPS late
-		player.setWallLife(MinuWall.LIFETIME);
+		player.setDelay(2); //sets the delay value back to the defaults.
+		player.setWallLife(MinuWall.LIFETIME);//sets the wall life to defaults.
 	}
 
 }
