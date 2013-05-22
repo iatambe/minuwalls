6ea264f1576c0ed7b0be1a66a36026a0f2f06fe6
indraneel.tambe.1 <indraneel.tambe.1@ada5487b-2e95-c5f6-e23e-4c46d1c84117>
2013-05-22 20:45:21 +0000

    Fixed something in InstructionManual.java

--- trunk/InstructionManual.java
+++ trunk/InstructionManual.java
@@ -44,24 +44,56 @@ public class InstructionManual {
     }
 
 	private void showInstructionDialog() {
-		dialogBox.showMessageDialog(null, "Welcome to MinuWalls, the best GridWorld game on Earth!\n"
+		int res;
+		res = dialogBox.showConfirmDialog(null, "Welcome to MinuWalls, the best GridWorld game on Earth!\n"
 			+ "Let's start with Game Instructions...",
-			"Welcome to MinuWalls!", JOptionPane.INFORMATION_MESSAGE);
-		dialogBox.showMessageDialog(null, "Avoid the walls that trail from your opponent's bike. "
+			"Welcome to MinuWalls!", JOptionPane.OK_CANCEL_OPTION,
+			JOptionPane.INFORMATION_MESSAGE);
+		if (res == JOptionPane.CANCEL_OPTION || res == JOptionPane.CLOSED_OPTION) {
+			dialogBox.showMessageDialog(null, "Okay then.", "MinuWalls", JOptionPane.PLAIN_MESSAGE);
+			return;
+		}
+		res = dialogBox.showMessageDialog(null, "Avoid the walls that trail from your opponent's bike. "
 			+ "It's similar to Tron, or Light Bike.",
-			"Instructions #1", JOptionPane.INFORMATION_MESSAGE);
-		dialogBox.showMessageDialog(null, "For Player 1, the controls are the Arrow Keys." + '\n' 
+			"Instructions #1", JOptionPane.OK_CANCEL_OPTION,
+			JOptionPane.INFORMATION_MESSAGE);
+		if (res == JOptionPane.CANCEL_OPTION || res == JOptionPane.CLOSED_OPTION) {
+			dialogBox.showMessageDialog(null, "Okay then.", "MinuWalls", JOptionPane.PLAIN_MESSAGE);
+			return;
+		}
+		res = dialogBox.showMessageDialog(null, "For Player 1, the controls are the Arrow Keys." + '\n' 
 			+ "The arrow keys' directions are self-explanatory.",
-			"Instructions #2", JOptionPane.INFORMATION_MESSAGE);
-		dialogBox.showMessageDialog(null, "For Player 2, the controls are WASD." + '\n' 
+			"Instructions #2", JOptionPane.OK_CANCEL_OPTION,
+			JOptionPane.INFORMATION_MESSAGE);
+		if (res == JOptionPane.CANCEL_OPTION || res == JOptionPane.CLOSED_OPTION) {
+			dialogBox.showMessageDialog(null, "Okay then.", "MinuWalls", JOptionPane.PLAIN_MESSAGE);
+			return;
+		}
+		res = dialogBox.showMessageDialog(null, "For Player 2, the controls are WASD." + '\n' 
 			+ "W moves up, A moves left, S moves down, D moves right.",
-			"Instructions #3", JOptionPane.INFORMATION_MESSAGE);
-		dialogBox.showMessageDialog(null, "There are powerups in the game as well. "
+			"Instructions #3", JOptionPane.OK_CANCEL_OPTION,
+			JOptionPane.INFORMATION_MESSAGE);
+		if (res == JOptionPane.CANCEL_OPTION || res == JOptionPane.CLOSED_OPTION) {
+			dialogBox.showMessageDialog(null, "Okay then.", "MinuWalls", JOptionPane.PLAIN_MESSAGE);
+			return;
+		}
+		res = dialogBox.showMessageDialog(null, "There are powerups in the game as well. "
 			+ "Collect them and you will be able to gain an advantage or disadvantage.",
-			"Instructions #4", JOptionPane.INFORMATION_MESSAGE);
-		dialogBox.showMessageDialog(null, "The game begins when the stoplight turns green. " + '\n'
+			"Instructions #4", JOptionPane.OK_CANCEL_OPTION,
+			JOptionPane.INFORMATION_MESSAGE);
+		if (res == JOptionPane.CANCEL_OPTION || res == JOptionPane.CLOSED_OPTION) {
+			dialogBox.showMessageDialog(null, "Okay then.", "MinuWalls", JOptionPane.PLAIN_MESSAGE);
+			return;
+		}
+		res = dialogBox.showMessageDialog(null, "The game begins when the stoplight turns green. " + '\n'
 			+ "The first person to reach 10 points wins!",
-			"Instructions #5", JOptionPane.INFORMATION_MESSAGE);
+			"Instructions #5", JOptionPane.OK_CANCEL_OPTION,
+			JOptionPane.INFORMATION_MESSAGE);
+		/*
+		if (res == JOptionPane.CANCEL_OPTION || res == JOptionPane.CLOSED_OPTION) {
+			dialogBox.showMessageDialog(null, "Okay then.", "MinuWalls", JOptionPane.PLAIN_MESSAGE);
+			return;
+		}*/
 		return;
 	}	
 
