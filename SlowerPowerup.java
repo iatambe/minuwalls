c3b49955bc8d6362f0e6ec4e12bf4ae2b39533a8
hans04219@gmail.com <hans04219@gmail.com@ada5487b-2e95-c5f6-e23e-4c46d1c84117>
2013-05-22 20:48:50 +0000

    edited comments

--- SlowerPowerup.java
+++ SlowerPowerup.java
@@ -3,14 +3,14 @@ import java.awt.Color;
 public class SlowerPowerup extends MinuPowerup {
  
 	public SlowerPowerup(){
-		this.setColor(Color.gray); //Gray color change
+		this.setColor(Color.gray); //sets color to gray.
 	}
      
 	public void act() {}
 
 	public void editPlayer(MinuWaller player) {
-		player.setDelay(6); //6 STEPS late
-		player.setWallLife(300); 
+		player.setDelay(6); //sets the delay to a higher value, making the MinuWaller, or player, be slower.
+		player.setWallLife(300); //makes the wall life left behind by the MinuWaller to higher so the walls will remain the same length.
 	}
 
 }
