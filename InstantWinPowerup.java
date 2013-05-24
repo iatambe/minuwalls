40691e261b01c949aff244b4d47f5a443099c7bb
indraneel.tambe.1@gmail.com <indraneel.tambe.1@gmail.com@ada5487b-2e95-c5f6-e23e-4c46d1c84117>
2013-05-24 20:11:22 +0000

    added comments to InstantWinPowerup.java

--- InstantWinPowerup.java
+++ InstantWinPowerup.java
@@ -2,21 +2,30 @@ import info.gridworld.actor.*;
 import info.gridworld.grid.*;
 import java.awt.Color;
 import java.util.*;
-//this powerup moves around on the grid, making it harder to get, because it is an automatic win powerup.
-public class InstantWinPowerup extends MinuPowerup {//extends abstract MinuPowerup
 
+/* When a player "eats" the InstantWinPowerup, that player's opponent explodes instantly,
+ * causing the first player to win. The InstantWinPowerup moves around randomly on the grid,
+ * making it harder to get.
+ */
+
+public class InstantWinPowerup extends MinuPowerup {//extends abstract MinuPowerup
+ /* this.count counts the steps before the InstantWinPowerup should
+  * make its next random move on the grid.
+  */
  private int count;
  private static int DELAY = 3; // DELAY is the amount of steps before the InstantWinPowerup moves.
- private Location prev;
+ private Location prev; // the previous location; is null at the start
  
  public InstantWinPowerup() {
   prev = null;
   count = 0;
-  this.setColor(Color.orange); //powerup is orange
+  this.setColor(Color.orange); // default color is Orange
  }
 
-
- public void act() {//overrides method of parent class
+ /* act() causes the InstantWinPowerup to move around randomly on the grid. It is designed
+  * to tend to follow a path and to avoid moving back-and-forth.
+  */
+ public void act() {
   count++;
   if (count == DELAY) { //We give it a three step latency <--- REFER TO: private static int DELAY 
    count = 0;
@@ -26,11 +35,13 @@ public class InstantWinPowerup extends MinuPowerup {//extends abstract MinuPower
   Random rand = new Random(); //random location selector
  
   Location cur = super.getLocation();
-  ArrayList<Location> locs = new ArrayList<Location>();
+  ArrayList<Location> locs = new ArrayList<Location>(); // list of possible move locations
   Grid<Actor> grid = this.getGrid();
-  for (int dir = 0; dir <= 315; dir += 45) { //gets all adjacent locations
+  
+   //get all adjacent locations
+  for (int dir = 0; dir <= 315; dir += 45) {
    Location next = cur.getAdjacentLocation(dir);
-   if (grid.isValid(next) && grid.get(next) == null) {//if the adj. location is valid, add it to a list of possible locations to move to
+   if (grid.isValid(next) && grid.get(next) == null) {
     locs.add(next);
    }
   }
@@ -42,18 +53,22 @@ public class InstantWinPowerup extends MinuPowerup {//extends abstract MinuPower
      break;
     }
    }
-   if (i < locs.size() && locs.size() > 1) {//remove the i+1th Location from the possible areas to move to, unless the Location is the only place to move to.
+   // removes the prev location as an option to move to IF it is not the only available option
+   if (i < locs.size() && locs.size() > 1) {
+    // if this point is reached, then i is the index of the previous location in locs
     locs.remove(i);
    }
 
-   int dirFacing = prev.getDirectionToward(cur);//direction it is facing is the direction from the previous Location to the current Location
-   Location next = cur.getAdjacentLocation(dirFacing);//next is the Location the powerup is facing.
+   int dirFacing = prev.getDirectionToward(cur);//direction this InstantWinPowerup is facing, from the previous location
+   Location next = cur.getAdjacentLocation(dirFacing);//next is the Location toward which the powerup is facing
    
    for (i = 0; i < locs.size(); i++) {
     if (locs.get(i).equals(next)) {
      break;
     }
    }
+   // There is a 1/3 chance that it will automatically choose the Location
+   // in the direction from the previous location.
    if (i < locs.size() && rand.nextInt(3) == 0) {
     prev = new Location(cur.getRow(), cur.getCol());
     super.moveTo(locs.get(i));
@@ -65,7 +80,10 @@ public class InstantWinPowerup extends MinuPowerup {//extends abstract MinuPower
   int index = rand.nextInt(locs.size());
   super.moveTo(locs.get(index));
  }
-
+ 
+ /* InstantWinPowerup's editPlayer() causes the player's opponent to be 
+  * "condemned" to death.
+  */
  public void editPlayer(MinuWaller player) {
   MinuWaller winner = player, loser = player.getOpponent();
   loser.setWillDie(true);
