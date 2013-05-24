65e2fc30fe8a16b41655a49ea2737bcccbc74e50
hans04219@gmail.com <hans04219@gmail.com@ada5487b-2e95-c5f6-e23e-4c46d1c84117>
2013-05-24 20:10:07 +0000

    edited commments

--- trunk/NormalizePowerup.java
+++ trunk/NormalizePowerup.java
@@ -1,6 +1,6 @@
 import java.awt.Color;
 
-public class NormalizePowerup extends MinuPowerup {//extends the abstract class
+public class NormalizePowerup extends MinuPowerup {//extends the abstract class, this makes the player normal again if they get a slower or faster powerup.
 
 	public NormalizePowerup(){
 		this.setColor(Color.magenta); //sets color to magenta
