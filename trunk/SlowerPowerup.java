cefae874fa36436cca82d249fe66783e1e09e21b
hans04219@gmail.com <hans04219@gmail.com@ada5487b-2e95-c5f6-e23e-4c46d1c84117>
2013-05-24 20:11:09 +0000

    edited commments

--- trunk/SlowerPowerup.java
+++ trunk/SlowerPowerup.java
@@ -1,12 +1,12 @@
 import java.awt.Color;
 
-public class SlowerPowerup extends MinuPowerup {
+public class SlowerPowerup extends MinuPowerup {//makes the player slower, extends abstract class MinuPowerup so it will implement needed methods.
  
 	public SlowerPowerup(){
 		this.setColor(Color.gray); //sets color to gray.
 	}
      
-	public void act() {}
+	public void act() {}//does nothing when act is called.
 
 	public void editPlayer(MinuWaller player) {
 		player.setDelay(6); //sets the delay to a higher value, making the MinuWaller, or player, be slower.
