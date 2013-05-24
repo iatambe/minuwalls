3d0d233c3ccb1f61e6a69734b37b3e7d3d809b26
indraneel.tambe.1@gmail.com <indraneel.tambe.1@gmail.com@ada5487b-2e95-c5f6-e23e-4c46d1c84117>
2013-05-24 20:26:14 +0000

    commented ExplosionBlock.java

--- trunk/ExplosionBlock.java
+++ trunk/ExplosionBlock.java
@@ -4,16 +4,28 @@ import info.gridworld.grid.*;
 import java.awt.*;
 import java.util.*;
 
+/* ExplosionBlock is an Actor that propagates outwards in a square until it reaches
+ * the boundaries of the world. ExplosionBlocks are created when a MinuWaller dies.
+ */
+
 public class ExplosionBlock extends Actor {//ExplosionBlocks are put into a grid when a MinuWaller crashes, resulting in a propagating square of ExplosionBlocks.
 
 	private boolean willExplode; //whether will explode or not in the current step
-	public void setWillExplode(boolean val) { this.willExplode = val; }//The getter of the previous method
+	public void setWillExplode(boolean val) { this.willExplode = val; }
 	public boolean getWillExplode() { return this.willExplode; }
+   
+  /* The direction variable (inherited from Actor) determines how the ExplosionBlock behaves.
+  * If direction is in the set {0, 90, 180, 270}, lateral directions,
+  * then the ExplosionBlock only creates a new ExplosionBlock in the Location in front of it and disappears.
+  * If direction is in the set {45, 135, 225, 315}, diagonal directions,
+  * then the ExplosionBlock creates new ExplosionBlocks in front of it and in the two Locations 
+  * adjacent to that location.
+  */
     
      public ExplosionBlock(int dir) {//takes the direction that it will explode in.
           super();
           this.setDirection(dir);
-		  this.willExplode = false;
+          this.willExplode = false;
      } 
      //We are creating the explosion that spreads throughout the grid
      private ExplosionBlock makeExpBlockAt(int direction, Location next) {
@@ -23,21 +35,30 @@ public class ExplosionBlock extends Actor {//ExplosionBlocks are put into a grid
           return block;
      } 
      
-     // act() does nothing for ExplosionBlocks
-     // instead of act(), MWWorld calls the propagate() method.
-    // public void act() } 
+  /* !! Instead of using the act() method, the ExplosionBlock has a propagate method. We did
+   * this so that we could use the method to return a value instead of keeping it void.
+   */
 
-/* propagate() causes the ExplosionBlocks to propagate outwards */
+/* propagate() causes the ExplosionBlocks to propagate outwards.
+ * It returns a list of the new ExplosionBlocks it has created.
+ * It is designed so that if 2 ExplosionBlocks collide (such as from two explosions happening at the same time),
+ * then the two ExplosionBlocks annihilate each other.
+ */
 public ArrayList<ExplosionBlock> propagate() { 
-     if (this.getGrid() == null || this.willExplode == true) //We're using a boolean statemment to contain the explosion and make sure it doesnt surpass the grid
+    /* If this ExplosionBlock is removed or has already been condemned to death, then
+     * exit from the method and return an empty list.
+     */
+     if (this.getGrid() == null || this.willExplode == true)
           return new ArrayList<ExplosionBlock>(); 
-     int dir = this.getDirection();  //We're getting the data and storing it in an array list. <---
-     Location loc = this.getLocation(); //<-----
-     Grid<Actor> gr = this.getGrid(); //<=---- ARRAY_LIST ITEMS
+     
+     int dir = this.getDirection(); 
+     Location loc = this.getLocation(); 
+     Grid<Actor> gr = this.getGrid(); 
      ArrayList<ExplosionBlock> list = new ArrayList<ExplosionBlock>();
      ExplosionBlock eb;
      
-     if (dir % 90 == 45) { // if dir is a "diagonal" direction
+     if (dir % 90 == 45) { // if dir is a "diagonal" direction,
+         // create the adjacent blocks
           Location next1 = loc.getAdjacentLocation(dir - 45);
           if (gr.isValid(next1)) {
                Actor a = gr.get(next1); 
@@ -60,6 +81,7 @@ public ArrayList<ExplosionBlock> propagate() {
           } 
      } 
      
+     // create the ExplosionBlock in the Location in front of this one.
      Location next = loc.getAdjacentLocation(dir);
      if (gr.isValid(next)) {
           Actor a = gr.get(next);
@@ -67,7 +89,7 @@ public ArrayList<ExplosionBlock> propagate() {
                eb = this.makeExpBlockAt(dir, next);
                list.add(eb);
           } else if (a != null && a instanceof ExplosionBlock) {
-			   	((ExplosionBlock)a).setWillExplode(true); //We don't want a cluttered area with blocks landing on other blocks so we check a condition.
+			   	((ExplosionBlock)a).setWillExplode(true);
 			}
      } 
      
