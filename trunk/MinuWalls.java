f570cadb9a7fd3e0571a20b87434adad31c27ce1
hans04219@gmail.com <hans04219@gmail.com@ada5487b-2e95-c5f6-e23e-4c46d1c84117>
2013-05-24 20:09:03 +0000

    edited commments

--- trunk/MinuWalls.java
+++ trunk/MinuWalls.java
@@ -1,17 +1,17 @@
 // Driver for MinuWalls.
-import javax.swing.JOptionPane; 
+import javax.swing.JOptionPane;//for instructions 
 
 
-public class MinuWalls {
+public class MinuWalls {//driver for our game. Sets up the MWWorld and displays it and instructions if needed.
      
 	public static void main(String [] args) {
-		JOptionPane optionPane = new JOptionPane();
-		System.setProperty("info.gridworld.gui.selection", "hide");
-		InstructionManual manual = new InstructionManual(optionPane);
-		manual.showInstructions();
+		JOptionPane optionPane = new JOptionPane();//makes window for instructions.
+		System.setProperty("info.gridworld.gui.selection", "hide");//if a person clicks on a square in the grid, it won't stay bolded.
+		InstructionManual manual = new InstructionManual(optionPane);//new InstructionManual in the window.
+		manual.showInstructions();//shows instructions.
 	
 		
-		MWWorld game = new MWWorld();
+		MWWorld game = new MWWorld();//makes the game.
 		game.startNew();
 		game.show();
      	
